ff5ee929cf8f547951109db3599ee76032f507eb
Hikarin <60568280+Hikari31768@users.noreply.github.com>
2025-12-09 14:49:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from astrbot.api.event.filter import event_message_type, EventMessageType
 logger = logging.getLogger(__name__)
 
 
-@register("astrbot_plugin_genshinimpact", "ましろSaber&Foolllll&Hikarin", "一个原神启动插件", "1.2.1", "https://github.com/Hikari31768/astrbot_plugin_genshinimpact")
+@register("astrbot_plugin_genshinimpact", "ましろSaber&Foolllll&Hikarin", "一个原神启动插件", "1.3", "https://github.com/Hikari31768/astrbot_plugin_genshinimpact")
 class GenshinImpactPlugin(Star):
     def __init__(self, context: Context, config: Optional[Dict] = None):
         super().__init__(context)
